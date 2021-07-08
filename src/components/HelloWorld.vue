5e123601f98eba562207aadd0ddffaf273975d64
Luhan <nameike1997@qq.com>
2021-07-09 00:07:18 +0800

    测试push

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="box">
+    <p>hellow vue</p>
+    <img
+      src="http://img.netbian.com/file/2020/0904/de2f77ed1090735b441ba5e4c2b460ca.jpg"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    console.log("hellow vue mounted");
+  },
+};
+</script>
+
+<style>
+</style>
